49e720b0a3b10ca48ed72d40125f17fe8f9b096b
sanskarpethe <87526905+sanskarpethe@users.noreply.github.com>
2021-07-27 08:57:54 +0530

    Add files via upload

---
+++ positive number.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+list1 = [52,54,-85,-5,8,5,-5,-85,69,54]
+ 
+for num in list1:
+
+    if num >= 0:
+        print( num )
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
